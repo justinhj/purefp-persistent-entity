b52ce8e718de2fa635107e36d6f7a0fe033d4c7a
Justin Heyes-Jones <justin.heyes-jones@yoppworks.com>
2020-01-23 17:39:01 -0800

    Getting it started

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pure FP persistent entity
+
+Part of a 3 part blog series on persistent enitities in Lagom, Monoids and using pure FP to implement a persistent entity of our own.
+
+
+
---
+++ src/main/scala/org/justinhj/Purefppersistententity.scala
@@ -0,0 +1,20 @@
+package org.justinhj
+
+// Created by https://github.com/justinhj/fp-starter-pack.g8
+// Simple ZIO App
+
+import zio.App
+import zio.console._
+
+object Purefppersistententity extends App {
+
+  def run(args: List[String]) =
+    myAppLogic.fold(_ => 1, _ => 0)
+
+  val myAppLogic =
+    for {
+      _ <- putStrLn("Hello! What is your name?")
+      n <- getStrLn
+      _ <- putStrLn(s"Hello, ${n}, welcome to ZIO!")
+    } yield ()
+}
